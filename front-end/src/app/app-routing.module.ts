14d4e0309c12f6ab16fb4615b95cf413566ce3eb
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-11-12 20:51:58 -0300

    ✨ feat: about component.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ComicsComponent } from './pages/comics/comics.component';
+import { AboutComponent } from './pages/about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'comics', component: ComicsComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ToggleThemeComponent } from './components/toggle-theme/toggle-theme.component';
 import { ComicsComponent } from './pages/comics/comics.component';
-import { ComicComponent } from './components/comic/comic.component';
 import { ComicItemComponent } from './components/comic-item/comic-item.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +17,11 @@ import { ComicItemComponent } from './components/comic-item/comic-item.component
     HeaderComponent,
     ToggleThemeComponent,
     ComicsComponent,
-    ComicComponent,
-    ComicItemComponent
+    ComicItemComponent,
+    AboutComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

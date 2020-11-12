669aec1d4ae8d1e7d6f5a13725a3e8bbef84a0f2
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-11-12 20:39:51 -0300

    ✨ feat: comics page component added.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ToggleThemeComponent } from './components/toggle-theme/toggle-theme.component';
+import { ComicsComponent } from './pages/comics/comics.component';
+import { ComicComponent } from './components/comic/comic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    ToggleThemeComponent
+    ToggleThemeComponent,
+    ComicsComponent,
+    ComicComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/pages/comics/comics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-comics',
+  templateUrl: './comics.component.html',
+  styleUrls: ['./comics.component.scss']
+})
+export class ComicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

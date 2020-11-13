9e357a42e9da60cfa0d7f23a65e468a7014fce35
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-13 09:12:09 -0300

    📦 refactor: header hover approach.

--- front-end/src/app/pages/comic-detail/comic-detail.component.ts
+++ front-end/src/app/pages/comic-detail/comic-detail.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Comic } from 'src/app/models/comic.model';
 
 @Component({
   selector: 'cms-comic-detail',
   templateUrl: './comic-detail.component.html',
-  styleUrls: ['./comic-detail.component.scss']
+  styleUrls: ['./comic-detail.component.scss'],
 })
 export class ComicDetailComponent implements OnInit {
+  @Input() comic: Comic;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

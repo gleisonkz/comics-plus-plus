da2564cadd159756211bed14d13ff7a3cdfab89f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-11-18 23:58:32 -0300

    ✨ feat: EFCore repository layer added.

---
+++ back-end/ComicWebAPI/Comic.RepositoryEF/Class/EFCoreRepository.cs
@@ -0,0 +1,37 @@
+﻿using Comic.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Comic.RepositoryEF.Class
+{
+    public class EFCoreRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ComicContext context;
+        private DbSet<T> dbSet;
+        private DbSet<T> DbSet => dbSet ??= context.Set<T>();
+
+        public EFCoreRepository(ComicContext context)
+        {
+            this.context = context;
+        }
+        public T Add(T entity)
+        {
+            return DbSet.Add(entity).Entity;
+        }
+
+        public void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public T Update(T entity)
+        {
+            return DbSet.Update(entity).Entity;
+        }
+
+        public IQueryable<T> GetQuery(bool trackingChanges = true)
+        {
+            return DbSet.AsNoTracking();
+        }
+    }
+}
---
+++ back-end/ComicWebAPI/Comic.RepositoryEF/Class/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proffy.Repository.Interfaces;
+
+namespace Comic.RepositoryEF.Class
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private readonly ComicContext context;
+
+        public UnityOfWork(ComicContext context)
+        {
+            this.context = context;
+        }
+
+        public int Commit()
+        {
+            return context.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            foreach (var entry in context.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Modified:
+                        entry.CurrentValues.SetValues(entry.OriginalValues);
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                }
+            }
+        }
+    }
+}

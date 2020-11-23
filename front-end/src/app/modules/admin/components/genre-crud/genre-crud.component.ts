2768f4a65015a1af538d540ce785eebe637f1af1
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-23 11:53:36 -0300

    💎 style: crud-table styles.

--- front-end/src/app/modules/admin/components/genre-crud/genre-crud.component.ts
+++ front-end/src/app/modules/admin/components/genre-crud/genre-crud.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { GenreDialogComponent } from '../dialogs/genre-dialog/genre-dialog.component';
 
 export interface Genre {
   genreID: number;
-  Name: string;
+  name: string;
 }
 
 @Component({
@@ -15,6 +17,9 @@ export interface Genre {
 export class GenreCrudComponent implements OnInit {
   form: FormGroup;
   genres: Genre[];
+  dataSource: MatTableDataSource<Genre>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['GenreID', 'Nome', 'Ações'];
   constructor(private dialogService: MatDialog) {}
 
   ngOnInit(): void {
@@ -22,7 +27,47 @@ export class GenreCrudComponent implements OnInit {
       genreID: new FormControl(''),
       name: new FormControl(''),
     });
-    this.genres = [{ genreID: 1, Name: 'Drama' }];
+    this.genres = [
+      { genreID: 1, name: 'Drama' },
+      // { genreID: 2, name: 'Action' },
+      // { genreID: 3, name: 'Romance' },
+      // { genreID: 4, name: 'Terror' },
+      // { genreID: 1, name: 'Drama' },
+      // { genreID: 2, name: 'Action' },
+      // { genreID: 3, name: 'Romance' },
+      // { genreID: 4, name: 'Terror' },
+      // { genreID: 1, name: 'Drama' },
+      // { genreID: 2, name: 'Action' },
+      // { genreID: 3, name: 'Romance' },
+      // { genreID: 4, name: 'Terror' },
+      // { genreID: 1, name: 'Drama' },
+      // { genreID: 2, name: 'Action' },
+      // { genreID: 3, name: 'Romance' },
+      // { genreID: 4, name: 'Terror' },
+    ];
+    this.dataSource = new MatTableDataSource<Genre>(this.genres);
+    setTimeout(() => {
+      this.paginator._intl.firstPageLabel = 'Primeira Página';
+      this.paginator._intl.lastPageLabel = 'Última Página';
+      this.paginator._intl.nextPageLabel = 'Próxima Página';
+      this.paginator._intl.previousPageLabel = 'Página Anterior';
+      this.paginator._intl.itemsPerPageLabel = 'Itens por página';
+      this.paginator._intl.getRangeLabel = function (page, pageSize, length) {
+        if (length === 0 || pageSize === 0) {
+          return '0 od ' + length;
+        }
+        length = Math.max(length, 0);
+        const startIndex = page * pageSize;
+        // If the start index exceeds the list length, do not try and fix the end index to the end.
+        const endIndex =
+          startIndex < length
+            ? Math.min(startIndex + pageSize, length)
+            : startIndex + pageSize;
+        return startIndex + 1 + ' - ' + endIndex + ' de ' + length;
+      };
+
+      this.dataSource.paginator = this.paginator;
+    }, 0);
   }
 
   openDialog() {
@@ -38,5 +83,12 @@ export class GenreCrudComponent implements OnInit {
     };
 
     this.dialogService.open(GenreDialogComponent, dialogConfig);
+  }
+
+  deleteGenre(item: Genre) {
+    console.log(item);
+  }
+  editGenre(item: Genre) {
+    console.log(item);
   }
 }

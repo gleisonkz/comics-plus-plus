ed699b1195eed39528abe2f78d591da14919cdf4
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-24 16:24:01 -0300

    ✨ feat: notification service added.

---
+++ front-end/src/app/services/notification.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  showMessage(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+}

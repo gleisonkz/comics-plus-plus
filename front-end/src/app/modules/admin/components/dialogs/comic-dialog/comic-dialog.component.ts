a5703b2bde26d8231693a73cfe617bb1fb1708dc
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-26 15:22:31 -0300

    ✨ feat: comic-dialog component generated.

---
+++ front-end/src/app/modules/admin/components/dialogs/comic-dialog/comic-dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './comic-dialog.component.html',
+  styleUrls: ['./comic-dialog.component.scss']
+})
+export class ComicDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

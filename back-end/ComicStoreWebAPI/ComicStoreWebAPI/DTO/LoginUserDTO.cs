8266ba21b5c4ae44c599ccfaaa4c858138bdf75d
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-12-05 20:35:40 -0300

    ✨ feat: DTO for login and register.

---
+++ back-end/ComicStoreWebAPI/ComicStoreWebAPI/DTO/LoginUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComicStore.Application.DTO
+{
+    public class LoginUserDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ back-end/ComicStoreWebAPI/ComicStoreWebAPI/DTO/RegisterUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComicStore.Application.DTO
+{
+    public class RegisterUserDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "As senhas não conferem")]
+        public string PasswordCheck { get; set; }
+    }
+}

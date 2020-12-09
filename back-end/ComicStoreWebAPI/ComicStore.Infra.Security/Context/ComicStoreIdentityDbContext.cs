446753892a77e6a30f97a341d4ee66a4072303e9
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-12-09 00:06:11 -0300

    📦 refactor: removing identity tables prefix.

--- back-end/ComicStoreWebAPI/ComicStore.Infra.Security/Context/ComicStoreIdentityDbContext.cs
+++ back-end/ComicStoreWebAPI/ComicStore.Infra.Security/Context/ComicStoreIdentityDbContext.cs
@@ -9,6 +9,15 @@ namespace ComicStore.Infra.EFRepository.Context
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.HasDefaultSchema("seg");
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
         }
         public ComicStoreIdentityDbContext(DbContextOptions<ComicStoreIdentityDbContext> options) : base(options) { }
 

241bd92dd1f690480c9767b5e6d12f669bbf81b3
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-12-11 14:55:37 -0300

    ✨ feat: order models first definiton.

--- back-end/ComicStoreWebAPI/ComicStore.Domain/POCO/Genre.cs
+++ back-end/ComicStoreWebAPI/ComicStore.Domain/POCO/Genre.cs
@@ -8,4 +8,25 @@ namespace ComicStore.Domain.POCO
         public string Description { get; set; }
         public virtual ICollection<Comic> Comics { get; set; }
     }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public int UserID { get; set; }
+    }
+
+    
+
+    public class OrderItem
+    {
+        public int OrderItemID { get; set; }
+        public int ComicID { get; set; }
+        public Comic Comic { get; set; }
+    }
 }

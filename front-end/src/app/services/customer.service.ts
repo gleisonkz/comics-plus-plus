9f7bea51508efae1a4d2eba8248a7e100808d5b0
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-12-17 16:42:02 -0300

    ✨ feat: getCustomerID.

--- front-end/src/app/services/customer.service.ts
+++ front-end/src/app/services/customer.service.ts
@@ -1,13 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthorizationService } from './authorization.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private jwtHelperService: JwtHelperService,
+    private authorizationService: AuthorizationService
+  ) {}
 
   // getCustomerAddress(customerID: number): Observable<Comic> {
   //   return this.http.get<Comic>(`${environment.apiURL}/comic/${comicID}`);
   // }
+
+  getCustomerID(): number {
+    const decodedToken = this.authorizationService.getDecodeToken();
+    const customerID = decodedToken && +decodedToken['customerID'];
+    return customerID;
+  }
 }

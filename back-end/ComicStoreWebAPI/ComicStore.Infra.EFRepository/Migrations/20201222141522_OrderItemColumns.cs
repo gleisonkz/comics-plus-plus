20deda5e8395dd288582cd16f1ba19be2b683f29
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-12-22 15:11:09 -0300

    ✨ feat: order-item properties.

---
+++ back-end/ComicStoreWebAPI/ComicStore.Infra.EFRepository/Migrations/20201222141522_OrderItemColumns.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComicStore.Infra.EFRepository.Migrations
+{
+    public partial class OrderItemColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "OrderItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "TotalValue",
+                table: "OrderItem",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "UnitValue",
+                table: "OrderItem",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "TotalValue",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "UnitValue",
+                table: "OrderItem");
+        }
+    }
+}

c0df5d6750aed44cc94a48deb482fffce2805208
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2021-01-06 11:40:01 -0300

    📦 refactor: removal unused files.

--- front-end/src/app/admin/classes/crud.interface.ts
+++
@@ -1,7 +0,0 @@
-import { FormGroup } from '@angular/forms';
-export interface BaseCrud {
-  openDialog(): void;
-  loadData<T>(obj: T): void;
-  form: FormGroup;
-  title: string;
-}
--- front-end/src/app/admin/classes/crud.token.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { BaseCrud } from './crud.interface';
-
-export const CRUD = new InjectionToken<BaseCrud>('BaseCrud');
